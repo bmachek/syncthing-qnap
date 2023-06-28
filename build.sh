31cd8592b54d752491873d2cb85b56db7f115317
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2023-06-28 16:35:03 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,75 @@
 #!/bin/bash
 
+# set -x
+
+if [ -z "$1" ] ; then
+    echo "Desired syncthing version missing."
+    echo "Example: $0 1.25.0"
+    exit 2
+fi
+
 # === Build own QPKG. ===
 APP_NAME="syncthing"
 APP_VERSION=$1
 APP_AUTHOR="Syncthing Community"
 APP_LICENSE="MPL-2.0"
 
-SUPPORTED_ARCHS=arm64 arm 386 amd64
+SUPPORTED_ARCHS="arm64 arm 386 amd64"
 
-if [ -z $APP_VERSION ] ; then
-    echo "Desired syncthing version missing."
-    echo "Example: $0 1.25.0"
+cd $(getcfg QDK Install_Path -f /etc/config/qpkg.conf)
+
+
+if [ -d "${APP_NAME}" ] ; then
+    echo "build.sh: Deleting pre-existing build directory"
+    rm -rf "${APP_NAME}"
 fi
 
-cd $(getcfg QDK Install_Path -f /etc/config/qpkg.conf`)
-
-rm -rf "${APP_NAME}"
-
+echo "build.sh: Creating new build directory"
 qbuild --create-env "${APP_NAME}"
 
-cd `getcfg QDK Install_Path -f /etc/config/qpkg.conf`/"${APP_NAME}"
+echo "build.sh: Downloading icons and configs"
+wget https://github.com/bmachek/syncthing-qnap/raw/master/icons_and_txts.zip -O temp.zip 2> /dev/null
+unzip -f temp.zip 2> /dev/null
+rm temp.zip 2> /dev/null
+
+echo "build.sh: Modifying build configuration"
+cd $(getcfg QDK Install_Path -f /etc/config/qpkg.conf)/"${APP_NAME}"
 sed -i -e "s/QPKG_NAME=.*/QPKG_NAME=\"${APP_NAME}\"/gI" "qpkg.cfg"
 sed -i -e "s/QPKG_VER=.*/QPKG_VER=\"${APP_VERSION}\"/gI" "qpkg.cfg"
 sed -i -e "s/QPKG_AUTHOR=.*/QPKG_AUTHOR=\"${APP_AUTHOR}\"/gI" "qpkg.cfg"
 sed -i -e "s/#QPKG_LICENSE=.*/QPKG_LICENSE=\"${APP_LICENSE}\"/gI" "qpkg.cfg"
 
-# 	3 (1+2): support both installation and migration
-sed -i -e "s/#QPKG_VOLUME_SELECT=.*/QPKG_VOLUME_SELECT=\"3\"/gI" "qpkg.cfg"
-#
-# sed -i -e "s/#QNAP_CODE_SIGNING/QNAP_CODE_SIGNING/gI" "qpkg.cfg"
-# sed -i -e "s/QNAP_CODE_SIGNING=.*/QNAP_CODE_SIGNING=\"1\"/gI" "qpkg.cfg"
-#
+echo "
+QDK_DATA_DIR_X19=\"arm\"
+# Location of files specific to arm-x31 packages.
+QDK_DATA_DIR_X31=\"arm\"
+# Location of files specific to arm-x41 packages.
+QDK_DATA_DIR_X41=\"arm\"
+# Location of files specific to x86 packages.
+QDK_DATA_DIR_X86=\"386\"
+# Location of files specific to x86 (64-bit) packages.
+QDK_DATA_DIR_X86_64=\"amd64\"
+# Location of file for arm64
+QDK_DATA_DIR_ARM_64=\"arm64\"
+" >> $(getcfg QDK Install_Path -f /etc/config/qpkg.conf)/"${APP_NAME}"/qpkg.cfg
 
-for ARCH IN $SUPPORTED_ARCHS ; do
-    rm -f syncthing/$ARCH/syncthing
-    curl https://github.com/syncthing/syncthing/releases/download/v$APP_VERSION/syncthing-linux-$ARCH-v$APP_VERSION.tar.gz | tar xzf - syncthing-linux-$ARCH-v$APP_VERSION/syncthing -C syncthing/$ARCH
+
+echo "build.sh: Downloading syncthing binaries"
+for ARCH in $SUPPORTED_ARCHS ; do
+    echo "  build.sh: ... for $ARCH"
+    mkdir $ARCH
+    wget -c https://github.com/syncthing/syncthing/releases/download/v$APP_VERSION/syncthing-linux-$ARCH-v$APP_VERSION.tar.gz -O - 2>/dev/null | tar -xz syncthing-linux-$ARCH-v$APP_VERSION/syncthing --strip-components 1
+    chmod +x syncthing
+    chown 0:0 syncthing
+    mv syncthing $ARCH
 done
 
-# Set permissions.
-find syncthing/ -type f -name syncthing -exec chmod +x {} \;
-
-#
-# Build QPKG.
+echo "build.sh: Starting QPKG build process"
 qbuild
 
-for ARCH IN $SUPPORTED_ARCHS ; do
-    if [ ! -f /share/CACHEDEV1_DATA/.qpkg/QDK/syncthing/build/syncthing_$APP_VERSION_$ARCH.qpkg ] ; then
-        echo "No package created for $ARCH, check output above."
-done
+echo "build.sh: QPKG build process finished"
 
-echo "Finished."
+echo "build.sh: Contents of build directory"
+ls -lh build/
+
+echo "build.sh: Finished."
